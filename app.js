8563d77383c4085e30c65e2e24d79543d8c873b7
Javier Palomero <jmpalomeros@gmail.com>
2022-10-12 19:15:54 +0200

    hasta iteracion 3.1

--- app.js
+++ app.js
@@ -7,10 +7,20 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
+//seccion de middlewares
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+
+//data a utilizar
+
+
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 // Register the location for handlebars partials here:
 
@@ -19,7 +29,41 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  
+  res.render('index', {
+    beerImage : "./images/beer.png"
+    })
 });
 
+app.get("/beers", (req,res)=>{
+  
+  punkApi.getBeers()
+  .then((beersInfo)=>{
+    console.log(beersInfo)
+    })
+  .catch((error)=>{
+    console.log(error)
+  })
+  res.render("beers", {
+    
+   
+
+
+  })
+})
+
+app.get("/random-beer",(req,res)=>{
+
+  res.render("random-beer",{
+
+  })
+})
+
+
+
+
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+
+
